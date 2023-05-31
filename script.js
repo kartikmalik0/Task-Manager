e58517ce8edacbb1c2ac8a56631f5343295c0548
Kartik <kartik20044@gmail.com>
2023-05-31 07:56:50 -0300

    Updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let listContainer = document.getElementById('list-container')
 
 function addTask() {
     if (inputBox.value === "") {
-        alert("You")
+        alert("You Have To Enter A Task")
     } else {
         let li = document.createElement('li');
         listContainer.appendChild(li);
@@ -35,57 +35,3 @@ function retData() {
     listContainer.innerHTML = localStorage.getItem("data")
 }
 retData()
-
-
-
-// const inputBox = document.getElementById("input-box")
-// const listContainer = document.getElementById('list-container')
-
-//   function addTask() {
-//     if (inputBox.value === "") {
-//         alert("You must write something!")
-
-//     } else {
-//         let li = document.createElement("li")
-//         li.innerHTML = inputBox.value
-//         listContainer.appendChild(li);
-//         let span = document.createElement('span');
-//         span.innerHTML = "&#10060;"
-//         li.appendChild(span)
-//     }
-//     inputBox.value = ""
-//     saveData()
-// }
-// listContainer.addEventListener('click', function(e) {
-//     if (e.target.tagName === "LI") {
-//         e.target.classList.toggle('checked')
-//         saveData()
-//     } else if (e.target.tagName === "SPAN") {
-//         e.target.parentElement.remove()
-//         saveData()
-//     }
-// }, false)
-
-// function saveData() {
-//     localStorage.setItem("data", listContainer.innerHTML);
-// }
-
-// function loadData() {
-//     listContainer.innerHTML = localStorage.getItem("data")
-// }
-// loadData()
-
-
-// const inputBox = document.getElementById("input-box");
-// const listContainer = document.getElementById("list-container");
-
-// function addTask() {
-//     if (inputBox.value === "") {
-//         alert("You must write something!");
-//     } else {
-//         let li = document.createElement("li");
-//         li.innerHTML = inputBox.value;
-//         listContainer.appendChild(li);
-//         inputBox.value = ""; // Clear the input box after adding the task
-//     }
-// }
